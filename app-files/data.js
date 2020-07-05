294e942283b3470b32d58b2603cbae92798e1b95
dayProgramadora <67766215+dayProgramadora@users.noreply.github.com>
2020-07-05 15:43:39 -0400

    tercer codigo

--- app-files/data.js
+++ app-files/data.js
@@ -1,45 +1,7 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-entrada-emergencias",
-      "name": "ENTRADA EMERGENCIAS",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1500,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.18516889384116375,
-          "pitch": 0.08459588657190409,
-          "rotation": 0,
-          "target": "1-sala-de-espera"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-sala-de-espera",
+      "id": "0-sala-de-espera",
       "name": "SALA DE ESPERA",
       "levels": [
         {
@@ -64,22 +26,22 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 2.67868118289877,
-          "pitch": 0.08219705202282412,
+          "yaw": 2.6109425391967545,
+          "pitch": 0.19243536225190105,
           "rotation": 0,
-          "target": "2-triage"
+          "target": "1-triage"
         },
         {
-          "yaw": 0.7692271670401567,
-          "pitch": 0.05141244272436829,
+          "yaw": 0.7927743006198931,
+          "pitch": 0.03453567558627846,
           "rotation": 0,
-          "target": "0-entrada-emergencias"
+          "target": "2-entrada-emergencias"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-triage",
+      "id": "1-triage",
       "name": "TRIAGE",
       "levels": [
         {
@@ -104,10 +66,44 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.914002340487606,
-          "pitch": 0.3128510412329,
-          "rotation": 6.283185307179586,
-          "target": "1-sala-de-espera"
+          "yaw": 1.9170024458329493,
+          "pitch": 0.16293961215920838,
+          "rotation": 0,
+          "target": "0-sala-de-espera"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-entrada-emergencias",
+      "name": "ENTRADA EMERGENCIAS",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.011242549426048853,
+          "pitch": 0.2763340139848758,
+          "rotation": 0,
+          "target": "0-sala-de-espera"
         }
       ],
       "infoHotspots": []
